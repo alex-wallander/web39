d7b44fcd60cac13c0ed4ee6c0076c4f721865467
alex-wallander <alexwallander@gmail.com>
2021-03-11 11:51:23 -0600

    wip

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+require('dotenv').config()
+
 console.log('web 39 rules big time')
+
+const express = require('express');
+
+const server = express()
+
+server.use(express.json());
+
+console.log(process.env.NODE_ENV)
+
+if(process.env.NODE_ENV !== 'development') { // on Heroku machine, an env variable is called "NODE_ENV" => "production"
+    const cors = require('cors');
+    server.use(cors());
+}
+
+server.use('*', (req, res) => {
+    res.send('<h1>success</h1>')
+})
+
+const PORT = process.env.PORT || 4000
+
+
+server.listen(PORT, () => {
+    console.log(`listening on ${PORT}`)
+})
